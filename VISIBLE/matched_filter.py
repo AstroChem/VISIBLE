d432cbf6e42b44fa831c219b21e8b37d3720c4ef
Ryan Loomis <rloomis@cfa.harvard.edu>
2018-01-23 12:46:57 -0500

    fixed filterfile loop

--- VISIBLE/matched_filter.py
+++ VISIBLE/matched_filter.py
@@ -434,249 +434,249 @@ def matched_filter(filterfile=None, datafile=None, mu_RA=0., mu_DEC=0., src_dist
             if outfile:
                 curr_outfile = outfile[filter_index]
 
-        #############################
-        #   Read the filter image   #
-        #############################
+            #############################
+            #   Read the filter image   #
+            #############################
 
-        # now that we have the data, let's import the filter file
-        if verbose:
-            print "Reading filter file " + str(filter_index) + " of " + str(nfilter) + ": " + curr_filterfile
-            t0 = time.time()
-
-        if isinstance(curr_filterfile, SkyImage):
-            filter_img = curr_filterfile
-        elif "image.out" in curr_filterfile:
-            if src_distance is None:
-                 print "A source distance in pc needs to be provided in order to process a RADMC3D image file"
-                 return 
-            else: filter_img = import_model_radmc(src_distance, curr_filterfile)
-        elif "fits" in curr_filterfile:
-            filter_img = import_model_fits(curr_filterfile)
-        else:
-            print "Not a valid filter image option. Please provide a FITS file, a RADMC3D image file, or a SkyImage object)."
-            return 
-
-        # the number of filter channels needs to be smaller than the data channels
-        if (len(filter_img.freqs) >= len(data.freqs)):
-            print "Number of channels in filter exceeds number of data channels. Filtering cannot continue."
-            return
-
-        elif (len(filter_img.freqs) >= len(data.freqs)*0.5):
-            print "WARNING: Number of channels in data file seems small compared to width of filter. Make sure there is adequate baseline in the data file."
-
-        if verbose: 
-            t1 = time.time()
-            print "Read filter image: " + datafile
-            print "Filter read time = " + str(t1-t0)
-
-
-
-        ##############################
-        #   Interpolate the filter   #
-        ##############################
-
-        # if interpolation enabled, then make filter match data resolution (in velocity space)
-        if interpolate:
+            # now that we have the data, let's import the filter file
             if verbose:
-                print "Interpolating filter"
+                print "Reading filter file " + str(filter_index+1) + " of " + str(nfilter) + ": " + curr_filterfile
                 t0 = time.time()
 
-            # determine the reference frequencies and freq spacings
-            filter_rfreq = np.mean(filter_img.freqs)
-            filter_delfreq = filter_img.freqs[1] - filter_img.freqs[0]
-
-            data_rfreq = np.mean(data.freqs)
-            data_delfreq = data.freqs[1] - data.freqs[0]
-
-            if data_delfreq < 0:
-                if filter_delfreq > 0:
-                    filter_img.data = filter_img.data[:,:,::-1]
-                    filter_delfreq = -filter_delfreq
+            if isinstance(curr_filterfile, SkyImage):
+                filter_img = curr_filterfile
+            elif "image.out" in curr_filterfile:
+                if src_distance is None:
+                     print "A source distance in pc needs to be provided in order to process a RADMC3D image file"
+                     return 
+                else: filter_img = import_model_radmc(src_distance, curr_filterfile)
+            elif "fits" in curr_filterfile:
+                filter_img = import_model_fits(curr_filterfile)
             else:
-                if filter_delfreq < 0:
-                    filter_img.data = filter_img.data[:,:,::-1]
-                    filter_delfreq = -filter_delfreq
+                print "Not a valid filter image option. Please provide a FITS file, a RADMC3D image file, or a SkyImage object)."
+                return 
 
-            filter_vwidth = filter_delfreq/filter_rfreq*c_kms
-            data_vwidth = data_delfreq/data_rfreq*c_kms
+            # the number of filter channels needs to be smaller than the data channels
+            if (len(filter_img.freqs) >= len(data.freqs)):
+                print "Number of channels in filter exceeds number of data channels. Filtering cannot continue."
+                return
 
-            nchan_filter = len(filter_img.freqs)
-            nchan_data = len(data.freqs)
-
-            chan_grid = np.arange(nchan_filter)
-            interp_chans = (np.arange(nchan_data)*data_vwidth/filter_vwidth)[(np.arange(nchan_data)*data_vwidth/filter_vwidth) <= np.max(chan_grid)]
-
-            interp_grid_x, interp_grid_y, interp_grid_chan = np.meshgrid(np.arange(filter_img.data.shape[0]), np.arange(filter_img.data.shape[1]), interp_chans)
-
-            interp_grid_x = np.ravel(interp_grid_x)
-            interp_grid_y = np.ravel(interp_grid_y)
-            interp_grid_chan = np.ravel(interp_grid_chan)
-
-            interp_data = ndimage.map_coordinates(filter_img.data, [interp_grid_y, interp_grid_x, interp_grid_chan], order=1)
-            interp_data = interp_data.reshape((filter_img.data.shape[0], filter_img.data.shape[1], interp_chans.shape[0]))
-
-            filter_img.data = interp_data
-            filter_img.freqs = ndimage.map_coordinates(filter_img.freqs, [interp_chans], order=1)
+            elif (len(filter_img.freqs) >= len(data.freqs)*0.5):
+                print "WARNING: Number of channels in data file seems small compared to width of filter. Make sure there is adequate baseline in the data file."
 
             if verbose: 
                 t1 = time.time()
-                print "Filter interpolated"
-                print "Filter interpolation time = " + str(t1-t0)
+                print "Read filter image: " + curr_filterfile
+                print "Filter read time = " + str(t1-t0)
 
 
 
-        #########################################
-        #   Calculate the filter visibilities   #
-        #########################################
+            ##############################
+            #   Interpolate the filter   #
+            ##############################
 
-        if verbose:
-            print "Generating kernel"
-            t0 = time.time()
+            # if interpolation enabled, then make filter match data resolution (in velocity space)
+            if interpolate:
+                if verbose:
+                    print "Interpolating filter"
+                    t0 = time.time()
 
-        nchan_kernel = len(filter_img.freqs)
+                # determine the reference frequencies and freq spacings
+                filter_rfreq = np.mean(filter_img.freqs)
+                filter_delfreq = filter_img.freqs[1] - filter_img.freqs[0]
 
-        kernel = np.empty(nchan_kernel*nvis, dtype='complex128').reshape(nvis, nchan_kernel)
-        kernel[:,:] = vis_sample(imagefile=filter_img, uu=data.uu, vv=data.vv, mu_RA=mu_RA, mu_DEC=mu_DEC)
+                data_rfreq = np.mean(data.freqs)
+                data_delfreq = data.freqs[1] - data.freqs[0]
 
-        # calculate the noise covariance matrix and its inverse
-        if window_func == "none":
-            R_inv = np.identity(nchan_kernel)
+                if data_delfreq < 0:
+                    if filter_delfreq > 0:
+                        filter_img.data = filter_img.data[:,:,::-1]
+                        filter_delfreq = -filter_delfreq
+                else:
+                    if filter_delfreq < 0:
+                        filter_img.data = filter_img.data[:,:,::-1]
+                        filter_delfreq = -filter_delfreq
 
-        else:
-            # now we assuming window_func is "Hanning"
-            if binfactor > 4:
-                # treat binning factors larger than 4 as having no channel correlation (valid for Hanning window function)
+                filter_vwidth = filter_delfreq/filter_rfreq*c_kms
+                data_vwidth = data_delfreq/data_rfreq*c_kms
+
+                nchan_filter = len(filter_img.freqs)
+                nchan_data = len(data.freqs)
+
+                chan_grid = np.arange(nchan_filter)
+                interp_chans = (np.arange(nchan_data)*data_vwidth/filter_vwidth)[(np.arange(nchan_data)*data_vwidth/filter_vwidth) <= np.max(chan_grid)]
+
+                interp_grid_x, interp_grid_y, interp_grid_chan = np.meshgrid(np.arange(filter_img.data.shape[0]), np.arange(filter_img.data.shape[1]), interp_chans)
+
+                interp_grid_x = np.ravel(interp_grid_x)
+                interp_grid_y = np.ravel(interp_grid_y)
+                interp_grid_chan = np.ravel(interp_grid_chan)
+
+                interp_data = ndimage.map_coordinates(filter_img.data, [interp_grid_y, interp_grid_x, interp_grid_chan], order=1)
+                interp_data = interp_data.reshape((filter_img.data.shape[0], filter_img.data.shape[1], interp_chans.shape[0]))
+
+                filter_img.data = interp_data
+                filter_img.freqs = ndimage.map_coordinates(filter_img.freqs, [interp_chans], order=1)
+
+                if verbose: 
+                    t1 = time.time()
+                    print "Filter interpolated"
+                    print "Filter interpolation time = " + str(t1-t0)
+
+
+
+            #########################################
+            #   Calculate the filter visibilities   #
+            #########################################
+
+            if verbose:
+                print "Generating kernel"
+                t0 = time.time()
+
+            nchan_kernel = len(filter_img.freqs)
+
+            kernel = np.empty(nchan_kernel*nvis, dtype='complex128').reshape(nvis, nchan_kernel)
+            kernel[:,:] = vis_sample(imagefile=filter_img, uu=data.uu, vv=data.vv, mu_RA=mu_RA, mu_DEC=mu_DEC)
+
+            # calculate the noise covariance matrix and its inverse
+            if window_func == "none":
                 R_inv = np.identity(nchan_kernel)
 
-            elif binfactor == 1:
-                diagonals = [2./3.*np.ones(nchan_kernel-1), np.ones(nchan_kernel), 2./3.*np.ones(nchan_kernel-1)]         
-                R = sparse.diags(diagonals, [-1, 0, 1], format='csc').toarray()
-                R_inv = np.linalg.inv(R)
+            else:
+                # now we assuming window_func is "Hanning"
+                if binfactor > 4:
+                    # treat binning factors larger than 4 as having no channel correlation (valid for Hanning window function)
+                    R_inv = np.identity(nchan_kernel)
 
-            elif binfactor == 2:
-                diagonals = [3./10.*np.ones(nchan_kernel-1), np.ones(nchan_kernel), 3./10.*np.ones(nchan_kernel-1)]     
-                R = sparse.diags(diagonals, [-1, 0, 1], format='csc').toarray()
-                R_inv = np.linalg.inv(R)
+                elif binfactor == 1:
+                    diagonals = [2./3.*np.ones(nchan_kernel-1), np.ones(nchan_kernel), 2./3.*np.ones(nchan_kernel-1)]         
+                    R = sparse.diags(diagonals, [-1, 0, 1], format='csc').toarray()
+                    R_inv = np.linalg.inv(R)
 
-            elif binfactor == 3:
-                diagonals = [1./6.*np.ones(nchan_kernel-1), np.ones(nchan_kernel), 1./6.*np.ones(nchan_kernel-1)]          
-                R = sparse.diags(diagonals, [-1, 0, 1], format='csc').toarray()
-                R_inv = np.linalg.inv(R)
+                elif binfactor == 2:
+                    diagonals = [3./10.*np.ones(nchan_kernel-1), np.ones(nchan_kernel), 3./10.*np.ones(nchan_kernel-1)]     
+                    R = sparse.diags(diagonals, [-1, 0, 1], format='csc').toarray()
+                    R_inv = np.linalg.inv(R)
 
-            elif binfactor == 4:
-                diagonals = [3./26.*np.ones(nchan_kernel-1), np.ones(nchan_kernel), 3./26.*np.ones(nchan_kernel-1)]     
-                R = sparse.diags(diagonals, [-1, 0, 1], format='csc').toarray()
-                R_inv = np.linalg.inv(R)
+                elif binfactor == 3:
+                    diagonals = [1./6.*np.ones(nchan_kernel-1), np.ones(nchan_kernel), 1./6.*np.ones(nchan_kernel-1)]          
+                    R = sparse.diags(diagonals, [-1, 0, 1], format='csc').toarray()
+                    R_inv = np.linalg.inv(R)
 
-        if verbose: 
-            t1 = time.time()
-            print "Kernel generated"
-            print "Kernel generation time = " + str(t1-t0)
+                elif binfactor == 4:
+                    diagonals = [3./26.*np.ones(nchan_kernel-1), np.ones(nchan_kernel), 3./26.*np.ones(nchan_kernel-1)]     
+                    R = sparse.diags(diagonals, [-1, 0, 1], format='csc').toarray()
+                    R_inv = np.linalg.inv(R)
 
-
-        ###############################
-        #   Do the actual filtering   #
-        ###############################
-
-        if verbose:
-            print "Starting kernel convolution"
-            t0 = time.time()
-
-        xc = np.zeros((data.VV.shape[1] - nchan_kernel + 1), dtype='complex128')  
-        kernel_noise_power = 0.
-    #    for i in np.arange(nvis):
-    #        xc += np.correlate(data.VV[i]*data.wgts[i], kernel[i])/np.sqrt(np.dot(kernel[i],kernel[i].conj())*np.sum(data.wgts[i]))
-
-        for v in np.arange(nvis):
-            # sketchy temporary check for flagged visibilities
-            if (not np.isnan(data.wgts[v])) and (data.wgts[v] > 0.00001):
-                xc += np.correlate(data.VV[v], np.matmul(data.wgts[v]*R_inv, kernel[v]))
-                kernel_noise_power += np.dot(kernel[v],np.matmul(data.wgts[v]*R_inv, kernel[v].conj()))
-
-        # normalize the output such that real and imag noise powers are both 1 (hence factor of sqrt(2))
-        xc = xc/np.sqrt(kernel_noise_power)*np.sqrt(2)
-
-        if verbose: 
-            t1 = time.time()
-            print "Data filtered"
-            print "Kernel convolution time = " + str(t1-t0)
-            print "max signal = " + str(np.max(np.real(xc))) + " sigma"
+            if verbose: 
+                t1 = time.time()
+                print "Kernel generated"
+                print "Kernel generation time = " + str(t1-t0)
 
 
-        ###############################
-        #   Calculate output x-axis   #
-        ###############################
+            ###############################
+            #   Do the actual filtering   #
+            ###############################
 
-        if mode=='channel':
-            response_chans = np.arange(xc.shape[0]) + nchan_kernel/2 + 0.5
-            x_axis = response_chans
+            if verbose:
+                print "Starting kernel convolution"
+                t0 = time.time()
 
-        elif mode=='frequency':
-            response_freqs = (np.squeeze(data.freqs[nchan_kernel/2:-nchan_kernel/2+1]) + data_delfreq/2.0)/1.e6
-            x_axis = response_freqs
+            xc = np.zeros((data.VV.shape[1] - nchan_kernel + 1), dtype='complex128')  
+            kernel_noise_power = 0.
+        #    for i in np.arange(nvis):
+        #        xc += np.correlate(data.VV[i]*data.wgts[i], kernel[i])/np.sqrt(np.dot(kernel[i],kernel[i].conj())*np.sum(data.wgts[i]))
 
-        else:
-            response_freqs = (np.squeeze(data.freqs[nchan_kernel/2:-nchan_kernel/2+1]) + data_delfreq/2.0)/1.e6
-            response_vels = (restfreq - response_freqs)/restfreq*c_kms
-            x_axis = response_vels
+            for v in np.arange(nvis):
+                # sketchy temporary check for flagged visibilities
+                if (not np.isnan(data.wgts[v])) and (data.wgts[v] > 0.00001):
+                    xc += np.correlate(data.VV[v], np.matmul(data.wgts[v]*R_inv, kernel[v]))
+                    kernel_noise_power += np.dot(kernel[v],np.matmul(data.wgts[v]*R_inv, kernel[v].conj()))
+
+            # normalize the output such that real and imag noise powers are both 1 (hence factor of sqrt(2))
+            xc = xc/np.sqrt(kernel_noise_power)*np.sqrt(2)
+
+            if verbose: 
+                t1 = time.time()
+                print "Data filtered"
+                print "Kernel convolution time = " + str(t1-t0)
+                print "max signal = " + str(np.max(np.real(xc))) + " sigma"
 
 
-
-
-        ############################
-        #   Plot filter response   #
-        ############################
-
-        if plot==True:
-
-            fig = pl.figure(figsize=(5,2.7), dpi=300)
-            ax = pl.axes([0.12,0.13,0.85,0.84])
+            ###############################
+            #   Calculate output x-axis   #
+            ###############################
 
             if mode=='channel':
-                pl.plot(response_chans, np.real(xc))
+                response_chans = np.arange(xc.shape[0]) + nchan_kernel/2 + 0.5
+                x_axis = response_chans
 
             elif mode=='frequency':
-                pl.plot(response_freqs, np.real(xc))
+                response_freqs = (np.squeeze(data.freqs[nchan_kernel/2:-nchan_kernel/2+1]) + data_delfreq/2.0)/1.e6
+                x_axis = response_freqs
 
             else:
-                pl.plot(response_vels, np.real(xc))
-
-            ax.minorticks_on()
-
-            pl.setp(ax.get_xticklabels(), size='9')
-            pl.setp(ax.get_yticklabels(), size='9')
-
-            pl.ylabel(r'Impulse response', size=9)
-            ax.yaxis.set_label_coords(-0.09, 0.5)
+                response_freqs = (np.squeeze(data.freqs[nchan_kernel/2:-nchan_kernel/2+1]) + data_delfreq/2.0)/1.e6
+                response_vels = (restfreq - response_freqs)/restfreq*c_kms
+                x_axis = response_vels
 
 
-            if mode=='channel':
-                pl.xlabel(r'MS channel', size=9)
 
-            elif mode=='frequency':
-                pl.xlabel(r'Frequency [MHz]', size=9)
 
+            ############################
+            #   Plot filter response   #
+            ############################
+
+            if plot==True:
+
+                fig = pl.figure(figsize=(5,2.7), dpi=300)
+                ax = pl.axes([0.12,0.13,0.85,0.84])
+
+                if mode=='channel':
+                    pl.plot(response_chans, np.real(xc))
+
+                elif mode=='frequency':
+                    pl.plot(response_freqs, np.real(xc))
+
+                else:
+                    pl.plot(response_vels, np.real(xc))
+
+                ax.minorticks_on()
+
+                pl.setp(ax.get_xticklabels(), size='9')
+                pl.setp(ax.get_yticklabels(), size='9')
+
+                pl.ylabel(r'Impulse response', size=9)
+                ax.yaxis.set_label_coords(-0.09, 0.5)
+
+
+                if mode=='channel':
+                    pl.xlabel(r'MS channel', size=9)
+
+                elif mode=='frequency':
+                    pl.xlabel(r'Frequency [MHz]', size=9)
+
+                else:
+                    pl.xlabel(r'Velocity [km s$^{-1}$]', size=9)
+
+                ax.xaxis.set_label_coords(0.5, -0.09)
+
+                pl.show()
+
+
+
+            ########################
+            #   Now return stuff   #
+            ########################
+
+            # simplest case is just writing to a file:
+            if outfile:
+                # save it
+                np.save(curr_outfile, np.vstack((x_axis, xc)))    
+            
+
+            # otherwise we're going to return the raw output of the filtering
             else:
-                pl.xlabel(r'Velocity [km s$^{-1}$]', size=9)
-
-            ax.xaxis.set_label_coords(0.5, -0.09)
-
-            pl.show()
-
-
-
-        ########################
-        #   Now return stuff   #
-        ########################
-
-        # simplest case is just writing to a file:
-        if outfile:
-            # save it
-            np.save(curr_outfile, np.vstack((x_axis, xc)))    
-        
-
-        # otherwise we're going to return the raw output of the filtering
-        else:
-            return np.vstack((x_axis, xc))
-
+                return np.vstack((x_axis, xc))
+        return
     return
