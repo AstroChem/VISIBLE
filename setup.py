22f0a707ba1415bf538d245615ac1b0cc32ef3c5
Ryan Loomis <rloomis@cvpost031.cv.nrao.edu>
2020-08-14 07:53:19 -0400

    finalizing new docs

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup (
     name = 'VISIBLE',
-    version = '0.2.3',
+    version = '0.2.4',
 
     # This package actually *is* zip-safe, but I've run into issues with
     # installing it as a Zip: in particular, the install sometimes fails with
